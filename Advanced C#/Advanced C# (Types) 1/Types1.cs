0ed49eb6cf8cbd2bca169648287b3b98f0d12e85
Dmitrii Chernov <dmitrii_chernov1@@epam.com>
2016-07-15 12:23:25 +0400

    first implementation

---
+++ Advanced C#/Advanced C# (Types) 1/Types1.cs
@@ -0,0 +1,35 @@
+﻿//Попытайтесь разорвать связь между классами через InfoData(класс Destination ничего не должен знать об этом типе)
+//
+//Оптимизируйте работу с точки зрения быстродействия.
+using System;
+
+namespace Types1
+{
+	struct InfoData
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+
+	class Source
+	{
+		internal void CheckAndProceed(List<InfoData> data)
+		{
+			var dest = new Destination();
+			//do something
+
+			dest.ProceedData(data);
+		}
+	}
+
+	class Destination
+	{
+		internal void ProceedData<T>(List<T> data) where T : struct
+		{
+			foreach (var item in data)
+			{
+				//do something
+			}
+		}
+	}
+}
---
+++ Advanced C#/Advanced C# (Types) 2/Types2.cs
@@ -0,0 +1,31 @@
+﻿//Вам необходимо  описать структуру, которая представляет собой товар в интернет-магазине.Товар имеет наименование, цену и количество едениц на складе.
+//
+//В дальнейшем вы будете работать со списком товара и осуществлять различные выборки с помощью LINQ to Object.
+//
+//Спроектируйте и опишите структуру правильно с точки зрения реализации ValueType.
+using System;
+
+namespace Types2
+{
+	struct Item
+	{
+		public Int32 Id { get; set; }
+		public String Name { get; set; }
+		public Decimal Price { get; set; }
+		public UInt32 Count { get; set; }
+
+		public override Boolean Equals(object obj)
+		{
+			if (obj is Item)
+			{
+				return this.Id == ((Item)obj).Id;
+			}
+			return false;
+		}
+
+		public override Int32 GetHashCode()
+		{
+			return Id.GetHashCode();
+		}
+	}
+}
