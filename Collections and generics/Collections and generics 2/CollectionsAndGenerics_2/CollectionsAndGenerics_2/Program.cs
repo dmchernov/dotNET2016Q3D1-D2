8ba8d98e574fbbc5355021940e625f48512547b7
Dmitrii Chernov <dmitrii_chernov1@@epam.com>
2016-08-01 17:20:13 +0400

    Projects was created

---
+++ Collections and generics/Collections and generics 2/CollectionsAndGenerics_2/CollectionsAndGenerics_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Реализуйте обобщенную коллекцию с функциональностью сравнения ее элементов на равенство с переданными объектом. Объект может являться любым типом данных. Обоснуйте свой выбор.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsAndGenerics_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
