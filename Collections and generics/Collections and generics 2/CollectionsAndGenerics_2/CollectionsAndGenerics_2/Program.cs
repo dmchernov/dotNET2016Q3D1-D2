bf6bc5b650e4217e0a323dbbaf754c8ac20b67cb
Dmitrii Chernov <dmitrii_chernov1@@epam.com>
2016-08-02 08:27:09 +0400

    Task comleted

--- Collections and generics/Collections and generics 2/CollectionsAndGenerics_2/CollectionsAndGenerics_2/Program.cs
+++ Collections and generics/Collections and generics 2/CollectionsAndGenerics_2/CollectionsAndGenerics_2/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,19 @@ namespace CollectionsAndGenerics_2
 	{
 		static void Main(string[] args)
 		{
+			int[] arr = new[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+			MyCollection<int> myCol = new MyCollection<int>(arr);
+			Console.WriteLine(myCol.Equals(8));
+
+			string[] strArr = new[] {"Ivan", "ivan", "Petr", "Sergey"};
+			MyCollection<string> myColStr = new MyCollection<string>(strArr);
+			Console.WriteLine(myColStr.Equals("Petr"));
+
+			Point[] pointArr = new[] {new Point(1, 2), new Point(4, 5),};
+			MyCollection<Point> myColPoint = new MyCollection<Point>(pointArr);
+			Console.WriteLine(myColPoint.Equals(new Point(4,5)));
+
+			Console.ReadKey();
 		}
 	}
 
