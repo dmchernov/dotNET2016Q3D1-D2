35f1d0afe990cacfc1fdf12ac355b830b1cf71ee
Dmitrii Chernov <dmitrii_chernov1@@epam.com>
2016-10-28 09:45:46 +0400

    Custom Serialization complete

---
+++ Serialization/Custom Serialization/Task/Serialization/OrderDataContractSurrogate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Task.DB;
+
+namespace Task.Serialization
+{
+	class OrderDataContractSurrogate : IDataContractSurrogate
+	{
+		public Type GetDataContractType(Type type)
+		{
+			if (type == typeof(Employee))
+				return typeof(Employee);
+			else if (type == typeof(Shipper))
+				return typeof(Shipper);
+			else if (type == typeof(Customer))
+				return typeof(Customer);
+			else if (type == typeof(IEnumerable<Order_Detail>))
+				return typeof(IEnumerable<Order_Detail>);
+			else if (type == typeof(IEnumerable<Order>))
+				return typeof(IEnumerable<Order>);
+			else if (type == typeof(Order))
+				return typeof(Order);
+			return type;
+		}
+
+		public object GetObjectToSerialize(object obj, Type targetType)
+		{
+			if (targetType == typeof(Employee))
+				return (Employee)obj;
+			else if (targetType == typeof(Customer))
+				return (Customer)obj;
+			else if (targetType == typeof(Order_Detail))
+				return (Order_Detail)obj;
+			else if (targetType == typeof(Shipper))
+				return (Shipper)obj;
+			else if (targetType == typeof(Customer))
+				return (Customer)obj;
+			else if (targetType == typeof(IEnumerable<Order_Detail>))
+				return (IEnumerable<Order_Detail>)obj;
+			else if (targetType == typeof(IEnumerable<Order>))
+				return (IEnumerable<Order>)obj;
+			else if (targetType == typeof(Order))
+				return (Order)obj;
+			else if (targetType == typeof(Category))
+				return (Category)obj;
+			return obj;
+		}
+
+		public object GetDeserializedObject(object obj, Type targetType)
+		{
+			return obj;
+		}
+
+		public object GetCustomDataToExport(MemberInfo memberInfo, Type dataContractType)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object GetCustomDataToExport(Type clrType, Type dataContractType)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void GetKnownCustomDataTypes(Collection<Type> customDataTypes)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Type GetReferencedTypeOnImport(string typeName, string typeNamespace, object customData)
+		{
+			throw new NotImplementedException();
+		}
+
+		public CodeTypeDeclaration ProcessImportedType(CodeTypeDeclaration typeDeclaration, CodeCompileUnit compileUnit)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Serialization/Custom Serialization/Task/SerializationSolutions.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task.DB;
+using Task.TestHelpers;
+using System.Runtime.Serialization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using Task.Serialization;
+
+namespace Task
+{
+	[TestClass]
+	public class SerializationSolutions
+	{
+		Northwind dbContext;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			dbContext = new Northwind();
+		}
+
+		[TestMethod]
+		public void SerializationCallbacks()
+		{
+			dbContext.Configuration.ProxyCreationEnabled = false;
+
+			var tester = new XmlDataContractSerializerTester<IEnumerable<Category>>(new NetDataContractSerializer(), true);
+			var categories = dbContext.Categories.ToList();
+
+			//var c = categories.First();
+
+			var t = (dbContext as IObjectContextAdapter).ObjectContext;
+			foreach (var category in categories)
+			{
+				t.LoadProperty(category, p => p.Products);
+			}
+
+			tester.SerializeAndDeserialize(categories);
+		}
+
+		[TestMethod]
+		public void ISerializable() // Complete
+		{
+			dbContext.Configuration.ProxyCreationEnabled = false;
+
+			var tester = new XmlDataContractSerializerTester<IEnumerable<Product>>(new NetDataContractSerializer(), true);
+			var products = dbContext.Products.ToList();
+
+			var t = (dbContext as IObjectContextAdapter).ObjectContext;
+			foreach (var product in products)
+			{
+				t.LoadProperty(product, p => p.Category);
+			}
+
+			tester.SerializeAndDeserialize(products);
+		}
+
+
+		[TestMethod]
+		public void ISerializationSurrogate()
+		{
+			dbContext.Configuration.ProxyCreationEnabled = false;
+
+			var surrogateSelector = new SurrogateSelector();
+			surrogateSelector.AddSurrogate(typeof(Order_Detail), new StreamingContext(), new OrderDetailsSerializationSurrogate());
+
+			var tester = new XmlDataContractSerializerTester<IEnumerable<Order_Detail>>(new NetDataContractSerializer() {SurrogateSelector = surrogateSelector}, true);
+			var orderDetails = dbContext.Order_Details.ToList();
+
+			var t = (dbContext as IObjectContextAdapter).ObjectContext;
+			foreach (var od in orderDetails)
+			{
+				t.LoadProperty(od, p => p.Product);
+				t.LoadProperty(od, p => p.Order);
+			}
+
+			tester.SerializeAndDeserialize(orderDetails);
+		}
+
+		[TestMethod]
+		public void IDataContractSurrogate()
+		{
+			dbContext.Configuration.ProxyCreationEnabled = true;
+			dbContext.Configuration.LazyLoadingEnabled = true;
+
+			var tester = new XmlDataContractSerializerTester<IEnumerable<Order>>(new DataContractSerializer(typeof(IEnumerable<Order>), new DataContractSerializerSettings() {DataContractSurrogate = new OrderDataContractSurrogate(), PreserveObjectReferences = true, DataContractResolver = new ProxyDataContractResolver() }), true);
+			var orders = dbContext.Orders.ToList();
+
+			var t = (dbContext as IObjectContextAdapter).ObjectContext;
+			foreach (var order in orders)
+			{
+				t.LoadProperty(order, o => o.Shipper);
+				t.LoadProperty(order, o => o.Customer);
+				t.LoadProperty(order, o => o.Employee);
+				t.LoadProperty(order, o => o.Order_Details);
+			}
+
+			tester.SerializeAndDeserialize(orders);
+		}
+	}
+}
